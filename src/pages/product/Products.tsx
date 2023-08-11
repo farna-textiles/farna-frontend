dfa37e403a4d2d98911317940550a726b0e740e7
Abdul Rafay <abdulrafayn001@gmail.com>
2023-08-11 16:39:04 -0700

    Revise components, API calls, and dependencies for Product
    - Updated EditProduct component to allow editing product details
    - Added functionality to select and display end uses for products
    - Integrated end use creation and selection in the EditProduct form
    - Added validation schema for product details
    - Improved UI and user experience in EditProduct form
    - Utilized react-query for fetching end uses and product data
    - Refactored code to improve readability and maintainability
    - Handled errors and notifications during end use creation
    - Created reusable Multiselect component for end use selection
    - Replace number inputs with string inputs for lotNo, noOfFilaments, denier fields
    - Implement infinite scrolling for the endUses dropdown
    - Add debounce to endUse search for smoother user experience
    - Refactor code for better readability and maintainability
    - Update validation schema to use regex for lotNo, type, and noOfFilaments fields
    - Replace deprecated 'danier' field with 'denier'
    - Update imports for new dependencies and components
    - Implement useCreateEndUse custom hook for adding endUses
    - Update API_URLS for endUses routes
    - Add support for custom formatting in GenericTable component
    - Refactor getAllProducts function for consistent parameter naming
    - Remove unnecessary code from getAllProductsTemp function
    - Update deleteProduct and getProduct API interaction functions
    - Create createProduct and updateProduct functions with proper type definitions
    - Refactor getAllEndUses function for consistent API endpoint
    - Add createEndUse function for creating new EndUse entries
    - Update project dependencies including "multiselect-react-dropdown"

--- src/pages/product/Products.tsx
+++ src/pages/product/Products.tsx
@@ -1,79 +1,80 @@
-import { useMemo, useState } from "react";
-import { ActionButton, TableColumn, ProductObject as Product } from "../../interfaces";
+import { useCallback, useMemo, useState } from 'react';
 import Edit from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
 import { Box, Typography } from '@mui/material';
-import GenericTable from "../../components/table/GenericTable";
-import { deleteProduct, getAllProducts, getAllProductsTemp } from "../../api/productApi";
-import EditProduct from "./EditProduct.jsx";
-import ScreenLoader from "../../components/screen-loader/ScreenLoader.js";
+import { useNavigate } from 'react-router';
+import {
+  ActionButton,
+  TableColumn,
+  ProductObject as Product,
+  EndUse,
+} from '../../interfaces';
+import GenericTable from '../../components/table/GenericTable';
+import { getAllProducts } from '../../api/productApi';
+import { useDeleteProduct } from '../../hooks/useProduct';
+import EndUsesList from './component/EndUsesList';
 
 const Products = () => {
+  const deleteProductMutation = useDeleteProduct();
 
-    const [modalStatus, setModalStatus] = useState(-1)
-    const [deleteRow, setDeleteRow] = useState(false)
-    const columns: TableColumn<Product>[] = useMemo(
-      () => [
-        { field: 'id', label: 'ID' },
-        { field: 'lotNo', label: 'Lot No' },
-        { field: 'danier', label: 'Danier' },
-        { field: 'type', label: 'Type' },
-        { field: 'noOfFilaments', label: 'No Of Filaments' },
-        { field: 'luster', label: 'Luster' },
-      ],
-      []
-    );
-    const handleCloseModal = () => {
-        setModalStatus(-1);
-      };
+  const endUsesList = useCallback(
+    (endUses: EndUse[]) => <EndUsesList endUses={endUses} />,
+    []
+  );
+  const navigate = useNavigate();
 
-    const actionButtons: ActionButton[] = useMemo(
-      () => [
-        {
-          icon: <Edit />,
-          onClick: (id: number) => {
-            setModalStatus(id)
-          },
+  const columns: TableColumn<Product>[] = useMemo(
+    () => [
+      { field: 'lotNo', label: 'Lot No' },
+      { field: 'denier', label: 'Denier' },
+      { field: 'type', label: 'Type' },
+      { field: 'noOfFilaments', label: 'No Of Filaments' },
+      { field: 'luster', label: 'Luster' },
+      {
+        field: 'endUses',
+        label: 'End Uses',
+        format: endUsesList,
+      },
+    ],
+    [endUsesList]
+  );
+
+  const actionButtons: ActionButton[] = useMemo(
+    () => [
+      {
+        icon: <Edit />,
+        onClick: (id: number) => navigate(`/products/${id}/edit`),
+      },
+      {
+        icon: <DeleteIcon />,
+        onClick: (id: number) => {
+          deleteProductMutation.mutateAsync(id);
         },
-        {
-          icon: <DeleteIcon />,
-          onClick: (id: number) => {
-            setDeleteRow(true);
-            deleteProduct(id)
-            .then(() => {
-              console.log("Deleteion of product : Sucess")
-              // TODO: Add Toast Messaeg
-            })
-            .catch((error) => console.log("Deleteion of product : Failed"))
-            .finally(() => { setDeleteRow(false) })
-          },
-        },
-      ], []
-    );
-  
-    return (
-      <Box sx={{ m: 4 }}>
-        <Typography variant="h4" component="div" gutterBottom>
-          Products
-        </Typography>
-        {deleteRow ? <ScreenLoader message={"Deletion of product in progress"}/> :
-        <>
-          {modalStatus !== -1 && <EditProduct open={modalStatus !== -1 ? true : false} onClose={handleCloseModal} data={modalStatus}/>}
-          <hr className="mb-12" />
-          <Box sx={{ my: 2 }}>
-            <GenericTable<Product>
-              tableName="Products"
-              columns={columns}
-              fetchData={getAllProductsTemp}
-              actionButtons={actionButtons}
-              addButtonLink="/product/new"
-              addButtonLabel="Create Product"
-            />
-          </Box>
-          </>
-        }
-      </Box>
-    );
-  };
-  
-  export default Products;
+      },
+    ],
+    [deleteProductMutation, navigate]
+  );
+
+  return (
+    <Box sx={{ m: 4 }}>
+      <Typography variant="h4" component="div" gutterBottom>
+        Products
+      </Typography>
+      <>
+        <hr className="mb-12" />
+        <Box sx={{ my: 2 }}>
+          <GenericTable<Product>
+            tableName="Products"
+            columns={columns}
+            fetchData={getAllProducts}
+            actionButtons={actionButtons}
+            addButtonLink="/product/new"
+            addButtonLabel="Create Product"
+          />
+        </Box>
+      </>
+    </Box>
+  );
+};
+
+export default Products;
--- src/pages/product/component/MultiSelect.tsx
+++ src/pages/product/component/MultiSelect.tsx
@@ -1,60 +1,75 @@
-import React, { useState } from 'react';
-import { FormControl, InputLabel, MenuItem, Select, SelectChangeEvent } from '@mui/material';
+import { useState } from 'react';
+import {
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  SelectChangeEvent,
+} from '@mui/material';
+import { MultiSelectProps } from '../../../interfaces';
 
-// Define the type for the selected options (optional but recommended)
-interface Option {
-  value: string;
-  label: string;
-}
+const MultiSelect = <T extends string>({
+  options,
+  title,
+  trigger,
+  newOptions,
+}: MultiSelectProps<T>) => {
+  const [selectedOptions, setSelectedOptions] = useState<T[]>([]);
 
-interface MultiSelectProps {
-  options: Option[];
-  title: string;
-  newOptions: Object[];
-  trigger: (event:string[]) => void;
-}
+  const handleSelectChange = (event: SelectChangeEvent<T[]>) => {
+    setSelectedOptions(event.target.value as T[]);
+    trigger(event.target.value as T[]);
+  };
 
-const MultiSelect: React.FC<MultiSelectProps> = ({ options, title, trigger, newOptions }) => {
-  const [selectedOptions, setSelectedOptions] = useState<string[]>([]);
-
-  const handleSelectChange = (event: SelectChangeEvent<string[]>) => {
-    setSelectedOptions(event.target.value as string[]);
-    trigger(event.target.value as string[])
+  const getOptionLabel = (value: T): string => {
+    const option = options.find((opt) => opt.value === value);
+    if (option) {
+      return option.label;
+    }
+    const newOption = newOptions?.find((opt) => opt.value === value);
+    return newOption ? newOption.label : '';
   };
 
   return (
-    <>
-        <FormControl sx={{ width: '100%' }}>
-        <InputLabel sx={{ background: 'white', paddingLeft: '5px', paddingRight: '5px' }}>{title}</InputLabel>
-        <Select
-            required
-            multiple
-            fullWidth
-            value={selectedOptions}
-            onChange={handleSelectChange}
-            renderValue={(selected) => (
-            <div className='space-x-2'>
-                {(selected as string[]).map((value) => (
-                <span className='bg-gray-300 rounded-full px-4 py-2 cursor-pointer transition-all duration-150 hover:bg-gray-400' key={value}>{value}</span>
-                ))}
-            </div>
-            )}
-        >
-            {options.map((option) => (
-            <MenuItem key={option.value} value={option.value}>
-                {option.label}
-            </MenuItem>
+    <FormControl sx={{ width: '100%' }}>
+      <InputLabel
+        sx={{ background: 'white', paddingLeft: '5px', paddingRight: '5px' }}
+      >
+        {title}
+      </InputLabel>
+      <Select
+        required
+        multiple
+        fullWidth
+        value={selectedOptions}
+        onChange={handleSelectChange}
+        renderValue={(selected) => (
+          <div className="space-x-2">
+            {(selected as T[]).map((value) => (
+              <span
+                className="bg-gray-300 rounded-full px-4 py-2 cursor-pointer transition-all duration-150 hover:bg-gray-400"
+                key={value}
+              >
+                {getOptionLabel(value)}
+              </span>
             ))}
-            {
-              newOptions.map((option) => {
-                return <MenuItem key={option.name} value={option.name}>
-                  {option.name}
-                </MenuItem>
-              })
-            }
-        </Select>
-        </FormControl>
-    </>
+          </div>
+        )}
+      >
+        {options.map((option) => (
+          <MenuItem key={option.value} value={option.value}>
+            {option.label}
+          </MenuItem>
+        ))}
+        {newOptions?.map((option) => {
+          return (
+            <MenuItem key={option.value} value={option.value}>
+              {option.label}
+            </MenuItem>
+          );
+        })}
+      </Select>
+    </FormControl>
   );
 };
 
