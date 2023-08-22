35750748d07771058abe168b01207095476e54dc
huraira <your.email@example.com>
2023-08-22 16:04:05 +0500

    "Created a MobileSidebar component for mobile screens
    >modified Sidebar component
    >modified Layout component
    "

---
+++ src/pages/navbar/MobileSidebar.tsx
@@ -0,0 +1,111 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable react/function-component-definition */
+import * as React from 'react';
+// import Box from '@mui/material/Box';
+import { Box } from '@mui/material';
+import SwipeableDrawer from '@mui/material/SwipeableDrawer';
+import Button from '@mui/material/Button';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import MenuTwoToneIcon from '@mui/icons-material/MenuTwoTone';
+import {
+  IconCalendarStats,
+  IconGauge,
+  IconPresentationAnalytics,
+  IconFileAnalytics,
+  IconAdjustments,
+  IconLogout,
+  IconLock,
+  IconAddressBook,
+} from '@tabler/icons-react';
+
+type Anchor = 'top' | 'left' | 'bottom' | 'right';
+
+// Your mockdata
+const mockdata = [
+  { label: 'Dashboard', Icon: IconGauge, link: '/' },
+  { label: 'All Customers', Icon: IconAddressBook, link: '/customers' },
+  { label: ' View Products', Icon: IconCalendarStats, link: '/products' },
+  {
+    label: 'All Orders',
+    Icon: IconCalendarStats,
+    link: '/recent-order',
+  },
+  { label: 'Analytics', Icon: IconPresentationAnalytics },
+  { label: 'Users', Icon: IconFileAnalytics, link: '/users' },
+  { label: 'Security', Icon: IconLock },
+  { label: 'Settings', Icon: IconAdjustments },
+];
+
+export default function SwipeableTemporaryDrawer() {
+  const [state, setState] = React.useState({
+    left: false,
+  });
+
+  const toggleDrawer =
+    (anchor: Anchor, open: boolean) =>
+    (event: React.KeyboardEvent | React.MouseEvent) => {
+      if (
+        event &&
+        event.type === 'keydown' &&
+        ((event as React.KeyboardEvent).key === 'Tab' ||
+          (event as React.KeyboardEvent).key === 'Shift')
+      ) {
+        return;
+      }
+
+      setState({ ...state, [anchor]: open });
+    };
+
+  const list = (anchor: Anchor) => (
+    <Box
+      sx={{ width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 250 }}
+      role="presentation"
+      onClick={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}
+    >
+      <List>
+        {mockdata.map((link, index) => (
+          <ListItemButton component="a" href={link.link} key={index}>
+            <ListItemIcon sx={{ color: 'primary.main' }}>
+              {link.Icon ? <link.Icon /> : null}
+            </ListItemIcon>
+            <ListItemText primary={link.label} />
+          </ListItemButton>
+        ))}
+      </List>
+      <Divider />
+      <List sx={{ marginTop: '200px' }}>
+        <ListItemButton>
+          <ListItemIcon sx={{ color: 'error.main' }}>
+            <IconLogout />
+          </ListItemIcon>
+          <ListItemText primary="Logout" />
+        </ListItemButton>
+      </List>
+    </Box>
+  );
+
+  return (
+    <div>
+      {(['left'] as const).map((anchor) => (
+        <React.Fragment key={anchor}>
+          <Button onClick={toggleDrawer(anchor, true)}>
+            <MenuTwoToneIcon />
+          </Button>
+          <SwipeableDrawer
+            anchor={anchor}
+            open={state[anchor]}
+            onClose={toggleDrawer(anchor, false)}
+            onOpen={toggleDrawer(anchor, true)}
+          >
+            {list(anchor)}
+          </SwipeableDrawer>
+        </React.Fragment>
+      ))}
+    </div>
+  );
+}
--- src/pages/navbar/Sidebar.tsx
+++ src/pages/navbar/Sidebar.tsx
@@ -1,6 +1,11 @@
+/* eslint-disable import/order */
+/* eslint-disable react/self-closing-comp */
 /* eslint-disable react/jsx-props-no-spreading */
-/* eslint-disable import/no-extraneous-dependencies */
-import { Navbar, Group, ScrollArea, createStyles, rem } from '@mantine/core';
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable prettier/prettier */
+
+import { Navbar, createStyles, rem } from '@mantine/core';
 import {
   IconCalendarStats,
   IconGauge,
@@ -10,10 +15,12 @@ import {
   IconLock,
   IconAddressBook,
 } from '@tabler/icons-react';
+import SwipeableTemporaryDrawer from './MobileSidebar';
 import logo from '../../../public/farna-logo.png';
 import LinksGroup from './LinksGroup';
 import UserButton from './UserButton';
 import { User } from '../../interfaces';
+import { useEffect, useState } from 'react';
 
 const mockdata = [
   { label: 'Dashboard', icon: IconGauge, link: '/' },
@@ -30,8 +37,8 @@ const mockdata = [
     label: 'Products',
     icon: IconCalendarStats,
     links: [
-      { label: 'All Products', link: '/products' },
       { label: 'Create Product', link: '/product/new' },
+      { label: 'View Products', link: '/products' },
     ],
   },
   {
@@ -58,26 +65,31 @@ const mockdata = [
 
 const useStyles = createStyles((theme) => ({
   navbar: {
-    height: '100vh',
     backgroundColor:
       theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.white,
-    paddingBottom: 0,
+    padding: '1rem',
+    fontSize: '16px',
   },
 
   header: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
     padding: theme.spacing.md,
     paddingTop: 0,
-    marginLeft: `calc(${theme.spacing.md} * -1)`,
-    marginRight: `calc(${theme.spacing.md} * -1)`,
     color: theme.colorScheme === 'dark' ? theme.white : theme.black,
     borderBottom: `${rem(1)} solid ${
       theme.colorScheme === 'dark' ? theme.colors.dark[4] : theme.colors.gray[3]
     }`,
   },
 
-  links: {
-    marginLeft: `calc(${theme.spacing.md} * -1)`,
-    marginRight: `calc(${theme.spacing.md} * -1)`,
+  logo: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+  toggleButton: {
+    fontSize: 24,
+    cursor: 'pointer',
   },
 
   linksInner: {
@@ -97,31 +109,56 @@ const useStyles = createStyles((theme) => ({
 const Sidebar = () => {
   const userInfoString = localStorage.getItem('userInfo');
   const userInfo: User = userInfoString ? JSON.parse(userInfoString) : null;
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 818);
+    };
+
+    handleResize();
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
 
   const { classes } = useStyles();
-  const links = mockdata.map((item) => (
-    <LinksGroup {...item} key={item.label} />
-  ));
 
   return (
-    <Navbar height={800} width={{ sm: 300 }} p="md" className={classes.navbar}>
+    <Navbar
+      p="md"
+      className={classes.navbar}
+      style={{ height: isMobile ? '100px' : '100vh' }}
+    >
       <Navbar.Section className={classes.header}>
-        <Group position="apart">
-          <img src={logo} alt="farna logo" />
-        </Group>
+        {isMobile && <SwipeableTemporaryDrawer />}
+        <div
+          className={`${classes.logo} md:flex`}
+          style={{ justifyContent: isMobile ? 'flex-end' : 'flex-start' }}
+        >
+          <img src={logo} alt="farna logo" width={isMobile ? '68' : ''} />
+        </div>
       </Navbar.Section>
 
-      <Navbar.Section grow className={classes.links} component={ScrollArea}>
-        <div className={classes.linksInner}>{links}</div>
-      </Navbar.Section>
+      {!isMobile && (
+        <Navbar.Section className={`md:block ${classes.linksInner}`}>
+          {mockdata.map((item) => (
+            <LinksGroup {...item} key={item.label} />
+          ))}
+        </Navbar.Section>
+      )}
 
-      <Navbar.Section className={classes.footer}>
-        <UserButton
-          image="https://images.unsplash.com/photo-1508214751196-bcfd4ca60f91?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=255&q=80"
-          name={userInfo?.username ?? ''}
-          email={userInfo?.email ?? ''}
-        />
-      </Navbar.Section>
+      {!isMobile && (
+        <Navbar.Section className={`md:block ${classes.footer}`}>
+          <UserButton
+            image="https://images.unsplash.com/photo-1508214751196-bcfd4ca60f91?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=255&q=80"
+            name={userInfo?.username ?? ''}
+            email={userInfo?.email ?? ''}
+          />
+        </Navbar.Section>
+      )}
     </Navbar>
   );
 };
