5bf4333dee676cd1720027873f9e005dce4096df
Abdul Rafay <abdulrafayn001@gmail.com>
2023-09-27 11:02:49 -0400

    chore: Add splitVendorChunkPlugin to Vite configuration
    - Import splitVendorChunkPlugin from 'vite'
    - Add splitVendorChunkPlugin to the list of plugins

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
 /// <reference types="vitest" />
 /// <reference types="vite/client" />
 /* eslint-disable import/no-extraneous-dependencies */
-import { defineConfig } from 'vite';
+import { defineConfig, splitVendorChunkPlugin } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), splitVendorChunkPlugin()],
   test: {
     globals: true,
     environment: 'jsdom',
